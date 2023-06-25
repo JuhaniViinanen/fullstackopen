94eb34dbca25b5df380dd1b879806a26170bb948
Juhani Viinanen <juhani.ilmari.viinanen@gmail.com>
2023-06-25 17:48:15 +0300

    2.5 done

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,42 +1,4 @@
-
-const Header = ({ name }) => {
-  return (
-    <h1>{name}</h1>
-  )
-}
-
-const Content = ({ parts }) => {
-  return (
-    <ul>
-      {parts.map(part => 
-        <Part key={part.id} name={part.name} exercises={part.exercises} />
-      )}
-    </ul>
-  )
-}
-
-const Part = ({ name, exercises }) => {
-  return (
-    <li>{name} {exercises}</li>
-  )
-}
-
-const Total = ({ parts }) => {
-  const total = parts.reduce((acc, part) => acc + part.exercises, 0)
-  return (
-    <p>Number of exercises {total}</p>
-  )
-}
-
-const Course = ({course}) => {
-  return (
-    <div>
-      <Header name={course.name}/>
-      <Content parts={course.parts}/>
-      <Total parts={course.parts}/>
-    </div>
-  )
-}
+import Course from "./components/Course"
 
 const App = () => {
   const courses = [
