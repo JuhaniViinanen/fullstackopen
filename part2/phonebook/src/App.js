d389577c15b2628852a8beea7890e0520c166794
Juhani Viinanen <juhani.ilmari.viinanen@gmail.com>
2023-07-01 15:34:11 +0300

    2.12 done

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -30,12 +30,16 @@ const App = () => {
     } else {
       const newPerson = { 
         name: newName, 
-        number: newNumber,
-        id: persons.length + 1
+        number: newNumber
       }
-      setPersons(persons.concat(newPerson))
-      setNewName("")
-      setNewNumber("")
+
+      axios
+        .post("http://localhost:3001/persons", newPerson)
+        .then(response => {
+          setPersons(persons.concat(response.data))
+          setNewName("")
+          setNewNumber("")
+        })
     }
   }
 
