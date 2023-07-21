7417d16f70d13e7b457b1ff6a0ed23f00d0e696a
Juhani Viinanen <juhani.ilmari.viinanen@gmail.com>
2023-07-21 17:32:20 +0300

    4.4 done

--- part4/tests/list_helper.test.js
+++ part4/tests/list_helper.test.js
@@ -1,6 +1,26 @@
 const listHelper = require("../utils/list_helper")
+const dummydata = require("./dummy_data")
 
 test("dummy returns 1", () => {
     const result = listHelper.dummy([])
     expect(result).toBe(1)
+})
+
+describe("total likes", () => {
+
+    test("of empty list is 0", () => {
+        const res = listHelper.totalLikes([])
+        expect(res).toBe(0)
+    })
+
+    test("of list with one element is equal to the likes of that element", () => {
+        const res = listHelper.totalLikes([dummydata[0]])
+        expect(res).toBe(dummydata[0].likes)
+    })
+
+    test("of a list with multiple elements is the sum of their likes", () => {
+        const res = listHelper.totalLikes(dummydata)
+        expect(res).toBe(36)
+    })
+
 })
