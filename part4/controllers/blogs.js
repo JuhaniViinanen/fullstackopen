c1415d8f2a6b62a63f9c08b1eabd9d90d85fe946
Juhani Viinanen <juhani.ilmari.viinanen@gmail.com>
2023-08-03 19:21:07 +0300

    4.19 done

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -1,6 +1,15 @@
 const blogsRouter = require("express").Router()
 const Blog = require("../models/blog")
 const User = require("../models/user")
+const jwt = require("jsonwebtoken")
+
+const getTokenFrom = request => {
+  const auth = request.get("authorization")
+  if (auth && auth.startsWith("bearer ")) {
+    return auth.replace("bearer ", "")
+  }
+  return null
+}
 
 blogsRouter.get("/", async (req, res) => {
   const blogs = await Blog.find({}).populate("user", { username: 1, name: 1 })
@@ -9,7 +18,11 @@ blogsRouter.get("/", async (req, res) => {
 
 blogsRouter.post("/", async (req, res, next) => {
   try {
-    const user = await User.findOne({})
+    const decodedToken = jwt.verify(getTokenFrom(req), process.env.CYPHER)
+    if (!decodedToken.id) {
+      return res.status(401).json({ error: "invalid token" })
+    }
+    const user = await User.findById(decodedToken.id)
     const blog = new Blog({
       ...req.body,
       user: user._id
@@ -17,7 +30,6 @@ blogsRouter.post("/", async (req, res, next) => {
     const savedBlog = await blog.save()
     user.blogs = user.blogs.concat(savedBlog._id)
     await user.save()
-    
     res.status(201).json(savedBlog)
   } catch(exception) {
     next(exception)
