adf69593a5e0c8df6782ede4cc5b05635c07106a
Juhani Viinanen <juhani.ilmari.viinanen@gmail.com>
2023-09-10 18:48:55 +0300

    5.7 done

--- part5/src/components/Blog.jsx
+++ part5/src/components/Blog.jsx
@@ -1,8 +1,45 @@
+import { useState } from "react"
+
+const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: "solid",
+    borderWidth: 1,
+    marginBottom: 5
+}
+
 const Blog = ({ blog }) => {
-    return (
+    const [detailsVisible, setDetailsVisible] = useState(false)
+
+    const handleLike = event => {
+        console.log(`liked the blog ${blog.title}`)
+    }
+
+    const simple = () => (
         <div>
-            {blog.title} {blog.author}
+            <p>{blog.title} {blog.author}</p>
+            <button onClick={() => setDetailsVisible(true)}>more</button>
         </div>
+    )
+
+    const details = () => (
+        <div>
+            <p>{blog.title} {blog.author}</p>
+            <p>{blog.url}</p>
+            <p>
+                {blog.likes}
+                <button onClick={handleLike}>like</button>
+            </p>
+            <p>{blog.user.username}</p>
+            <button onClick={() => setDetailsVisible(false)}>less</button>
+        </div>
+    )
+
+    return (
+        <div style={blogStyle}>
+            {detailsVisible ? details() : simple()}
+        </div>
+
     )
 }
 
