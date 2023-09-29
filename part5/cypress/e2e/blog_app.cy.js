9511e732fd4197be8bc0f53ee57f05ba7fd8029b
Juhani Viinanen <juhani.ilmari.viinanen@gmail.com>
2023-09-29 20:24:40 +0300

    5.18 done

--- part5/cypress/e2e/blog_app.cy.js
+++ part5/cypress/e2e/blog_app.cy.js
@@ -1,6 +1,11 @@
 describe("Blog app", function() {
   beforeEach(function() {
     cy.request("POST", "http://localhost:3003/api/testing/reset")
+    cy.request("POST", "http://localhost:3003/api/users", {
+      "username": "squarebob117",
+      "password": "supersecret",
+      "name": "Spongebob"
+    })
     cy.visit("http://localhost:5173")
   })
 
@@ -9,5 +14,36 @@ describe("Blog app", function() {
     cy.get("#div-login-username")
     cy.get("#div-login-password")
     cy.get("#button-login")
+  })
+
+  describe("Login", function() {
+    it("succeeds with right credentials", function() {
+      cy.contains("username")
+        .find("input")
+        .type("squarebob117")
+      cy.contains("password")
+        .find("input")
+        .type("supersecret")
+      cy.contains("login")
+        .click()
+      cy.contains("Spongebob logged in")
+    })
+
+    it("fails with wrong crendetials", function() {
+      cy.contains("username")
+        .find("input")
+        .type("squarebob117")
+      cy.contains("password")
+        .find("input")
+        .type("password1234")
+      cy.contains("login")
+        .click()
+      cy.get(".errorMessage")
+        .should("contain", "invalid username or password")
+        .and("have.css", "color", "rgb(255, 0, 0)")
+
+      cy.get("html")
+        .should("not.contain", "Spongebob logged in")
+    })
   })
 })
